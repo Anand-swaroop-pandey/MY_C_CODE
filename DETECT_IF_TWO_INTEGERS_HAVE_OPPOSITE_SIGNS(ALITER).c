1dff351550cfa1b642c4cabf580c18e18a2e2edb
ANAND_PANDEY <anandswarooppandey.0199@gmail.com>
2020-10-26 04:13:20 +0530

    Add files via upload

---
+++ DETECT_IF_TWO_INTEGERS_HAVE_OPPOSITE_SIGNS(ALITER).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter Two numbers:\n");
+    scanf("%d%d",&a,&b);
+    int result=opposite_signs(a,b);
+    if(result==1)
+        printf("YES");
+    else if(result==0)
+        printf("NO");
+    return 0;
+}
+int opposite_signs(int a,int b)
+{
+    int XOR=a^b;
+    XOR=XOR>> 31;
+    int bit=XOR &1;
+    return bit;
+}
